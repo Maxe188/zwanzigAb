1de60fb4d4f278c5ab1dd27aea41c8d0db240aa1
Maxe188 <99894962+Maxe188@users.noreply.github.com>
2025-01-06 02:03:50 +0100

    maby bab

--- public/script.js
+++ public/script.js
@@ -184,12 +184,8 @@ document.getElementById('tradeButton').onclick = () => {
     socket.emit('enterTrade', selectedTradingCards);
 }
 outButton.onclick = () => {
-    if(lastGameState.dealingPlayerName === username){
-        alert('du darfst nicht tauschen');
-        return;
-    }
     console.log(Object.entries(lastGameState.otherPlayers));
-    if(!(Object.entries(lastGameState.otherPlayers).every((pair) => pair[1].traded))){
+    if(lastGameState.dealingPlayerName === username && !(Object.entries(lastGameState.otherPlayers).every((pair) => (pair[1] !== 'you' || pair[1].traded)))){
         alert('sicher das alle anderen vor dir getauscht haben?');
         return;
     }
