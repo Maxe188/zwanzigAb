e0dbcae171c342be87e7836ab6e9de61893f5052
Maxe188 <99894962+Maxe188@users.noreply.github.com>
2025-03-14 22:32:08 +0100

    creating class/ template

---
+++ classes/Bot.js
@@ -0,0 +1,14 @@
+class Bot {
+    name = 'bot';
+    constructor(name) {
+        this.name = name;
+    }
+
+    chooseTrumpf(cards) {
+        return card; // index??
+    }
+
+    chooseCard(center, hand) {
+        return card; // index??
+    }
+}
